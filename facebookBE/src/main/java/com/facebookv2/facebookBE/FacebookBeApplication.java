3b0057242ccecb7e6c7a26c8338f18f4c8ab0d0a
HLan2004 <155617010+HLan2004@users.noreply.github.com>
2025-08-28 19:39:56 +0700

    first commit

---
+++ facebookBE/src/main/java/com/facebookv2/facebookBE/FacebookBeApplication.java
@@ -0,0 +1,13 @@
+package com.facebookv2.facebookBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookBeApplication.class, args);
+	}
+
+}
---
+++ facebookBE/src/test/java/com/facebookv2/facebookBE/FacebookBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facebookv2.facebookBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
